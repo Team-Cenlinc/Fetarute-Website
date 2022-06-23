36d2143f731310d4ca24501ac78f7f40e11a157a
Sinapole <sinapole@fetarute.org>
2022-06-23 14:34:55 -0400

    添加部分未加入文件

---
+++ src/views/serverStatus.vue
@@ -0,0 +1,11 @@
+<template>
+  <BodyPictureViewer />
+</template>
+
+<script>
+import BodyPictureViewer from "@/components/bodyPictureViewer";
+export default {
+  name: 'HomeView',
+  components: {BodyPictureViewer},
+}
+</script>
