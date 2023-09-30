1a4a5dab77c811ed0b4f2846cee60c92b1c63578
Acatine <sinapole@fetarute.org>
2023-09-30 19:01:48 -0400

    More features for dark-mode.

--- src/main.js
+++ src/main.js
@@ -9,3 +9,19 @@ new Vue({
   i18n,
   vuetify,
 }).$mount('#app')
+
+let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+handleDarkMode(darkModeMediaQuery);
+function handleDarkMode(e) {
+  let darkModeOn = e.matches;
+  let favicon = document.querySelector('link[rel="icon"]');
+  if (!favicon) {
+    return;
+  }
+  if (darkModeOn) {
+    favicon.href = '/favicon_dark.ico';
+  } else {
+    favicon.href = '/favicon.ico';
+  }
+}
+darkModeMediaQuery.addEventListener('change', handleDarkMode);
